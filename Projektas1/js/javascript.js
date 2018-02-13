7dbea2bc33ea516e0a64623d90fd47d9372a8bfc
Laurynas <l.butrimavicius@gmail.com>
2018-02-13 16:10:14 +0200

    first commit

---
+++ Projektas1/js/javascript.js
@@ -0,0 +1,22 @@
+document.querySelector('.raudonas').addEventListener('click',function () {
+    var value = document.querySelector('.raudonas').innerText;
+    var newValue = parseInt(value) + 1;
+    document.querySelector('.raudonas').innerText = newValue;
+
+});
+
+document.querySelector('.geltonas').addEventListener('click',function () {
+    var value = document.querySelector('.geltonas').innerText;
+    var newValue = parseInt(value) + 1;
+    document.querySelector('.geltonas').innerText = newValue;
+
+});
+
+document.querySelector('.zalias').addEventListener('click', function () {
+
+    var x = document.querySelector('.geltonas').innerText;
+    var y = document.querySelector('.raudonas').innerText;
+
+
+
+});
