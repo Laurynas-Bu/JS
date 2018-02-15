a8b6948fd32d200155fa2eba7f404bb647929c8a
Laurynas B <l.butrimavicius@gmail.com>
2018-02-15 20:34:50 +0200

    New

---
+++ Projektas4-masinos/script/js.js
@@ -0,0 +1,49 @@
+var figuros = [
+    {ilgis: 2, plotis: 5, aukstis: 3},
+    {ilgis: 3, plotis: 2, aukstis: 6},
+    {ilgis: 1, plotis: 5, aukstis: 5}
+];
+
+document.querySelector('#primary-data').addEventListener('click',function () {
+
+    printData(figuros);
+
+
+});
+
+function printData(data) {
+
+var table = document.querySelector('.table');
+
+var tbody=document.querySelector(".table tbody");
+if (tbody != null){
+    tbody.remove();
+}
+
+var tableBody= document.createElement('tbody');
+
+for (var i=0; i < data.length; i++){
+    var tableBodyRow = document.createElement('tr');
+
+    var td = document.createElement('td');
+    td.appendChild(document.createTextNode(data [i].ilgis));
+    td.classList.add("text-center");
+    tableBodyRow.appendChild(td);
+
+    var td = document.createElement('td');
+    td.appendChild(document.createTextNode(data [i].plotis));
+    td.classList.add("text-center");
+    tableBodyRow.appendChild(td);
+
+    var td = document.createElement('td');
+    td.appendChild(document.createTextNode(data [i].aukstis));
+    td.classList.add("text-center");
+    tableBodyRow.appendChild(td);
+
+    tableBody.appendChild(tableBodyRow);
+
+}
+
+table.appendChild(tableBody);
+
+}
