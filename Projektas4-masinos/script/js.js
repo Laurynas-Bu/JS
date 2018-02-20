9ea06442dd6ec9f510be0fda93a3f94d35ffdb40
Laurynas <l.butrimavicius@gmail.com>
2018-02-20 12:37:51 +0200

    New

--- Projektas4-masinos/script/js.js
+++ Projektas4-masinos/script/js.js
@@ -4,10 +4,11 @@ var figuros = [
     {ilgis: 1, plotis: 5, aukstis: 5}
 ];
 
+//#primary-data
+
 document.querySelector('#primary-data').addEventListener('click',function () {
 
     printData(figuros);
-
 
 });
 
@@ -15,7 +16,7 @@ function printData(data) {
 
 var table = document.querySelector('.table');
 
-var tbody=document.querySelector(".table tbody");
+var tbody = document.querySelector(".table tbody");
 if (tbody != null){
     tbody.remove();
 }
@@ -45,5 +46,49 @@ for (var i=0; i < data.length; i++){
 }
 
 table.appendChild(tableBody);
+
+}
+
+document.querySelector('#primary-data').addEventListener('click',function () {
+
+    printMax(figuros);
+
+});
+
+function printMax(data) {
+
+    var table = document.querySelector('.table');
+
+    var tbody = document.querySelector(".table tbody");
+    if (tbody != null){
+        tbody.remove();
+    }
+
+    var tableBody= document.createElement('tbody');
+
+    for (var i=0; i < data.length; i++){
+
+        var tableBodyRow = document.createElement('tr');
+
+        var td = document.createElement('td');
+        td.appendChild(document.createTextNode(data [i].ilgis));
+        td.classList.add("text-center");
+        tableBodyRow.appendChild(td);
+
+        var td = document.createElement('td');
+        td.appendChild(document.createTextNode(data [i].plotis));
+        td.classList.add("text-center");
+        tableBodyRow.appendChild(td);
+
+        var td = document.createElement('td');
+        td.appendChild(document.createTextNode(data [i].aukstis));
+        td.classList.add("text-center");
+        tableBodyRow.appendChild(td);
+
+        tableBody.appendChild(tableBodyRow);
+
+    }
+
+    table.appendChild(tableBody);
 
 }
