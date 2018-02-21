1285a7dcf3bc807c50ac977683069de79d2bfdde
Laurynas <l.butrimavicius@gmail.com>
2018-02-21 13:54:15 +0200

    New

--- Projektas6 - modalinis langas/script/js.js
+++ Projektas6 - modalinis langas/script/js.js
@@ -1,27 +1,31 @@
+var modal, btn, span, clean;
 
-// Get the modal
-var modal = document.getElementById('myModal');
+modal = document.getElementById('myModal');
+btn = document.getElementById("myBtn");
+span = document.getElementsByClassName("close")[0];
+clean = document.getElementById('cleanto');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
-}
+};
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
-}
+};
 
-// When the user clicks anywhere outside of the modal, close it
+clean.onclick = function () {
+    modal.style.display = "none";
+};
+
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+};
+
+    if (modal.style.display=="none", true);
+{
+    document.querySelector('input-group-addon').value = "";
 }
+
 
