4ffffbd4995fe0cd916056b8a51dc393b148f355
Laurynas <l.butrimavicius@gmail.com>
2018-02-22 17:17:57 +0200

    New

--- Projektas6 - modalinis langas/script/js.js
+++ Projektas6 - modalinis langas/script/js.js
@@ -1,13 +1,17 @@
-var modal, btn, span, clean, forma;
+var modal, btn, span, clean, forma, body, addTo;
 
 modal = document.getElementById('myModal');
 btn = document.getElementById("myBtn");
 span = document.getElementsByClassName("close")[0];
 clean = document.getElementById('cleanto');
 forma = document.getElementById('forma');
+body = document.getElementById('body');
+addTo = document.getElementById('addto');
 
 btn.onclick = function() {
     modal.style.display = "block";
+   // overflowFunction()
+
 };
 
 span.onclick = function() {
@@ -31,3 +35,36 @@ function resetFunction() {
        forma.reset();
 }
 
+//function overflowFunction() {
+    //body.style.overflow = "hidden";
+//}
+
+
+
+addTo.addEventListener("click", function() {
+    required();
+});
+
+
+function required()
+{
+    var empty = document.form.text.value;
+    if (empty === "")
+    {
+        alert("Please input a Value");
+        return false;
+    }
+    else
+    {
+
+        var addIn;
+        addIn = document.getElementById("forma").elements[0].value;
+        document.getElementById(".col-md-4 person row").innerHTML = addIn;
+
+        //var node = document.createElement("div");
+        //var textnode = document.createTextNode(addIn);
+        //node.appendChild(textnode);
+        //document.getElementById(".col-md-4 person row").appendChild(node);
+        return true;
+    }
+}
